348a66b282d00723612c56825dcb76b5ea90fe96
RAMAKRISHNA3488 <132547350+RAMAKRISHNA3488@users.noreply.github.com>
2024-08-30 14:59:55 +0530

    Ram

---
+++ ems-backend/ems-backend/src/main/java/com/employeesystem/emsbackend/entity/LogInEmployee.java
@@ -0,0 +1,31 @@
+package com.employeesystem.emsbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogInEmployee {
+	private String firstName;
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
